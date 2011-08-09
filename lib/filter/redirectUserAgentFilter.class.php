a3f72e3d8e605cc66bc2521ccede5966c1a04bbf
Roel Sint <roel@beeldspraak.com>
2011-08-09 15:55:24 +0200

    added stopRoutes + remember a parameter stop in the user session

--- lib/filter/redirectUserAgentFilter.class.php
+++ lib/filter/redirectUserAgentFilter.class.php
@@ -2,8 +2,8 @@
 /**
  * Redirect request if it was issued by one of the configured user agents
  * params:
- *  - array userAgents
- *  - string redirectUrl (may contain a %s sprintf placeholder)
+ * - array userAgents
+ * - string redirectUrl (may contain a %s sprintf placeholder)
  *
  * Example filters.yml integration:
  *
@@ -22,85 +22,136 @@ redirectUserAgent:
  */
 class redirectUserAgentFilter extends sfFilter
 {
+  /**
+   * Matched user agent for the request
+   * @var mixed
+   */
+  protected $matchedUserAgent = null;
+  
+
   public function execute($filterChain)
   {
+    
+    // Execute this filter only once
+    if ( $this->isFirstCall() ) {
+      $context = $this->getContext();
+      $user = $context->getUser();
+      $request = $context->getRequest();
+      $routing = $context->getRouting();
+      
+      // check if a redirect stop is stored in the user session
+      if ( self::hasStopForUser($user) ) {
+        // proceed filter chain
+        $filterChain->execute();
+        return;
+      }
+      
+      // check matching user agent
+      $userAgent = $this->matchesUserAgentForRequest($request);
 
-  	// Execute this filter only once
-    if ($this->isFirstCall())
-    {
-	    $context  = $this->getContext();
-	    $request  = $context->getRequest();
-
-	    foreach ($this->getStopParameters() as $stopParameter) {
-	    	if ($request->hasParameter($stopParameter)) {
-	    		$filterChain->execute();
-	    		$content = $this->context->getResponse()->getContent();
-	    		$content = self::addStopParameterToLinks($content, $stopParameter);
-	    		$this->context->getResponse()->setContent($content);
-	    		return;
-	    	}
-	    }
-
-	    $redirectUrl = $this->getRedirectUrl();
-	    $userAgent = strtolower($request->getHttpHeader('User-Agent'));
-	    $userAgents = $this->getUserAgents();
-	    foreach ($userAgents as $checkAgent) {
-	    	if (false !== strpos($userAgent, $checkAgent)) {
-		    	if (sfConfig::get('sf_logging_enabled'))
-					{
-					  $context->getLogger()->info("Mobile user agent '$checkAgent' detected. Redirecting to '$redirectUrl'");
-					}
-	    		$context->getController()->redirect($redirectUrl, 0, 302);
-	    		throw new sfStopException();
-	    	}
-	    }
+      if ($userAgent) {
+        // check for stop parameters
+        foreach ($this->getStopParameters() as $stopParameter) {
+          if ( $request->hasParameter($stopParameter) ) {
+            self::setStopForUser($user, $userAgent);
+            // proceed filter chain
+            $filterChain->execute();
+            return;
+          }
+        }
+        
+        // check for stop routes
+        foreach ($this->getStopRoutes() as $stopRoute) {
+          if ( $routing->getCurrentRouteName() === $stopRoute ) {
+            // proceed filter chain
+            $filterChain->execute();
+            return;
+          }
+        }
+      
+        // no stops found, redirect user
+        $redirectUrl = $this->getRedirectUrl();
+        if ( sfConfig::get('sf_logging_enabled') ) {
+          $context->getLogger()->info("Mobile user agent '$userAgent' detected. Redirecting to '$redirectUrl'");
+        }
+        $context->getController()->redirect($redirectUrl, 0, 302);
+        throw new sfStopException();
+      }
     }
-
+    
     $filterChain->execute();
-
+    
     $content = $this->context->getResponse()->getContent();
-    $content = $this->addJavascriptRedirect($content, $userAgents, $redirectUrl);
+    $content = $this->addJavascriptRedirect($content, $this->getUserAgents(), $this->getRedirectUrl());
     $this->context->getResponse()->setContent($content);
-
+  
   }
 
   protected function addJavascriptRedirect($content, $userAgents, $redirectUrl)
   {
-  	$script = '
+    $script = '
   	<script type="text/javascript">
-  		var userAgents = '.json_encode($userAgents).';
+  		var userAgents = ' . json_encode($userAgents) . ';
   		var userAgent = navigator.userAgent
   		if (userAgent) {
 				for(var i=0; i<useragents .length; i++) {
 					var regex = new RegExp(userAgents[i], "i");
 					if (regex.exec(userAgent)) {
-						document.location.href=decodeURIComponent("'.urlencode($redirectUrl).'");
+						document.location.href=decodeURIComponent("' . urlencode($redirectUrl) . '");
 					}
 				}
   		}
   	</script>';
+    
+    return str_replace('<head>', '</head><head>' . $script, $content);
+  }
 
-  	return str_replace('<head>', '</head><head>'.$script, $content);
+  public static function setStopForUser(sfUser $user, $userAgent)
+  {
+    $user->setAttribute('stop', $userAgent, 'bs_mobile');
+  }
+
+  public static function resetStopForUser(sfUser $user)
+  {
+    $user->getAttributeHolder->remove('stop', 'bs_mobile');
+  }
+
+  public static function hasStopForUser(sfUser $user)
+  {
+    return $user->getAttribute('stop', false, 'bs_mobile') ? true : false;
+  }
+
+  public static function isMobileDeviceForUser(sfUser $user)
+  {
+    return self::hasStopForUser($user);
+  }
+  
+  public static function getMobileDeviceForUser(sfUser $user)
+  {
+    return $user->getAttribute('stop', false, 'bs_mobile');
   }
 
   /**
-   * We want to preserve the stopParemetrs so we have to append it to all links
-   *
-   * @param unknown_type $stopParameter
-   * @return unknown_type
+   * Check if one of the configured user agents matches
+   * 
+   * @param sfRequest $request
+   * @return mixed first matching user agent, FALSE if none matches
    */
-  public static function addStopParameterToLinks($content, $stopParameter)
+  protected function matchesUserAgentForRequest(sfRequest $request)
   {
-    //search and replace URLs
-    $pattern = '/(<a .+href|action)="([^"]+)"/i';
-    $replacement = '$1="$2?'.$stopParameter.'=1"';
-    $content = preg_replace($pattern, $replacement, $content);
-
-    //search and replace urls that already had an querystring parameter and thus now have two questionmarks in url
-    $pattern = '/(<a.+href|action)="([^?]+)\?(.*)\?'.$stopParameter.'=1"/i';
-    $replacement = '$1="$2?$3&'.$stopParameter.'=1"';
-    $content = preg_replace($pattern, $replacement, $content);
-    return $content;
+    if (is_null($this->matchedUserAgent))
+    {
+      $this->matchedUserAgent = false;
+      $userAgent = strtolower($request->getHttpHeader('User-Agent'));
+      $userAgents = $this->getUserAgents();
+      foreach ($userAgents as $checkAgent) {
+        if ( false !== strpos($userAgent, $checkAgent) ) {
+          $this->matchedUserAgent = $checkAgent;
+        }
+      }
+    }
+    
+    return $this->matchedUserAgent;
   }
 
   /**
@@ -124,7 +175,7 @@ class redirectUserAgentFilter extends sfFilter
    */
   protected function getRedirectUrl()
   {
-  	return $this->getParameter('redirectUrl');
+    return $this->getParameter('redirectUrl');
   }
 
   /**
@@ -136,6 +187,18 @@ class redirectUserAgentFilter extends sfFilter
    */
   protected function getStopParameters()
   {
-  	return $this->getParameter('stopParameters', array());
+    return $this->getParameter('stopParameters', array());
+  }
+
+  /**
+   * Returns a list of route names that should stop/suppress redirection
+   *
+   * Uses stopRoutes parameter configured in filters.yml but may be overwritten
+   *
+   * @return array
+   */
+  protected function getStopRoutes()
+  {
+    return $this->getParameter('stopRoutes', array());
   }
 }
